1cb96e10c85951f1da124e6a7bc70dee0875bd18
suji0028 <154439826+suji0028@users.noreply.github.com>
2024-11-26 17:30:50 +0530

    Update config.php

--- ADMIN/config.php
+++ ADMIN/config.php
@@ -1,6 +1,7 @@
 <?php
 
 $con = mysqli_connect("localhost", "root", "", "db_tour-and-travels");
+$con = mysqli_connect("localhost", "root", "", "merged_database");
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
